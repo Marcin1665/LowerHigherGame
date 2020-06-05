5ff03e612b5a1db9c2431a0f1e3f05b879d6b90f
Marcin1665 <marcin_tuszynski@hotmail.com>
2020-06-05 15:49:59 +0200

    fix

--- app/src/main/java/com/tuszynski/lowerhighergame/MainActivity.java
+++ app/src/main/java/com/tuszynski/lowerhighergame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tuszynski.lowerhighergame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,12 +14,13 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    public static final int MAX_NUMBER = 1000;
+    private static Intent intent;
     public static final Random RANDOM = new Random();
     private TextView msgTv;
     private EditText numberEnteredEt;
     private Button validate;
     private int numberToFind, numberTries;
+    public static final int A = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         numberEnteredEt = (EditText) findViewById(R.id.numberEnteredEt);
         validate = (Button) findViewById(R.id.validate);
         validate.setOnClickListener(this);
+
+
 
         newGame();
     }
@@ -47,6 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "Udało ci się znaleźć liczbę " + numberToFind + "!" +
                     " w " + numberTries + " próbach",
                     Toast.LENGTH_SHORT).show();
+            Intent intent2 = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(intent2);
             newGame();
         } else if (n > numberToFind) {
             msgTv.setText(R.string.too_high);
@@ -56,12 +62,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (3 == numberTries){
             Toast.makeText(this, "Niestety nie udało ci się, poprawna liczba to " + numberToFind,
                     Toast.LENGTH_SHORT).show();
+            Intent intent2 = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(intent2);
             newGame();
         }
     }
 
     private void newGame() {
-        numberToFind = RANDOM.nextInt(MAX_NUMBER) + 1;
+        numberToFind = RANDOM.nextInt(A) + 1;
         msgTv.setText(R.string.start_msg);
         numberEnteredEt.setText("");
         numberTries = 0;
--- app/src/main/java/com/tuszynski/lowerhighergame/StartActivity.java
+++ app/src/main/java/com/tuszynski/lowerhighergame/StartActivity.java
@@ -1,14 +1,45 @@
 package com.tuszynski.lowerhighergame;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class StartActivity extends AppCompatActivity {
+
+    private TextView startView;
+    private EditText rangeNumber;
+    private Button start;
+    public static final String MAX_NUMBER = "com.tuszynski.application.lowerhighergame.MAX_NUMBER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        start = findViewById(R.id.goToNextActivity);
+        rangeNumber = findViewById(R.id.range);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                moveToGameActivity();
+            }
+        });
     }
+
+    private void moveToGameActivity() {
+        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+
+        int oneUseVariable = Integer.parseInt(rangeNumber.getText().toString());
+        intent.putExtra(MAX_NUMBER,oneUseVariable);
+        startActivity(intent);
+    }
+
 }
