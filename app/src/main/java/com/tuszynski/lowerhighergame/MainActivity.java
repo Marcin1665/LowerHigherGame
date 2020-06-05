c3a7614042c01ed1f28be5f366812f308a89e366
Marcin1665 <marcin_tuszynski@hotmail.com>
2020-06-05 16:22:53 +0200

    fix

--- app/src/main/java/com/tuszynski/lowerhighergame/MainActivity.java
+++ app/src/main/java/com/tuszynski/lowerhighergame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tuszynski.lowerhighergame;
 
 import android.content.Intent;
+import android.content.SearchRecentSuggestionsProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,13 +15,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private static Intent intent;
-    public static final Random RANDOM = new Random();
+    public Random RANDOM = new Random();
     private TextView msgTv;
     private EditText numberEnteredEt;
     private Button validate;
     private int numberToFind, numberTries;
-    public static final int A = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         numberEnteredEt = (EditText) findViewById(R.id.numberEnteredEt);
         validate = (Button) findViewById(R.id.validate);
         validate.setOnClickListener(this);
+//        Intent intent = getIntent();
+//        int A = intent.getIntExtra(StartActivity.MAX_NUMBER, 2);
+
 
 
 
@@ -48,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         numberTries++;
 
         if (n == numberToFind) {
-            Toast.makeText(this, "Udało ci się znaleźć liczbę " + numberToFind + "!" +
+            Toast.makeText(this, "Udało ci się znaleźć liczbę " + numberToFind +
                     " w " + numberTries + " próbach",
                     Toast.LENGTH_SHORT).show();
             Intent intent2 = new Intent(MainActivity.this, StartActivity.class);
@@ -69,6 +72,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void newGame() {
+        Intent intent = getIntent();
+        int A = intent.getIntExtra(StartActivity.MAX_NUMBER, 2);
         numberToFind = RANDOM.nextInt(A) + 1;
         msgTv.setText(R.string.start_msg);
         numberEnteredEt.setText("");
